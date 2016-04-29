3d7c55eac2fb77547149f367d9a495237a769a61
Thijs van der Vossen <thijs@fngtps.com>
2016-04-29 15:17:12 +0200

    Add some smooth scrolling.

--- web/default.js
+++ web/default.js
@@ -7,3 +7,37 @@
     quote.removeClass('innerhover')
   })
 })()
+
+;(function() {
+  if (window.pageYOffset && history.pushState) {
+    function easeInOutCubic(t) {
+      return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
+    }
+    
+    function scroll(hash) {
+      var duration = 500, start = window.pageYOffset, end = $(hash).offset().top, now = Date.now(), frame = window.requestAnimationFrame || function(fn) { window.setTimeout(fn, 15) }
+      function step() {
+        var position, elapsed = Date.now() - now
+        if (elapsed > duration)
+          position = end
+        else
+          position = start + (end - start) * easeInOutCubic(elapsed / duration)
+        window.scroll(0, position)
+        if (elapsed > duration)
+          location.replace(hash)
+        else
+          frame(step)
+      }
+      step()
+    }
+  
+    $('a[href^="#"]:not([href="#"])').each(function() {
+      $(this).on('click', function(e) {
+        e.preventDefault()
+        if (location.hash !== this.hash)
+          window.history.pushState(null, null, this.hash)
+        scroll(this.hash)
+      })
+    })
+  }
+})()
